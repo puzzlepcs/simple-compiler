254e33167007b53fb79835bfe21b4fba90307b6c
ChonChon <barn1004@naver.com>
2018-10-08 23:59:56 -0700

    renamed files and modified some codes for C-Minus

--- scan.c
+++ scan.c
@@ -11,7 +11,7 @@
 
 /* states in scanner DFA */
 typedef enum
-   { START,INASSIGN,INCOMMENT,INNUM,INID,DONE }
+   { START,INEQ,INCOMMENT,INNUM,INID,DONE,INLT,INGT,INNE,INOVER,INCOMMENT_ }
    StateType;
 
 /* lexeme of identifier or reserved word */
@@ -56,8 +56,8 @@ static struct
     { char* str;
       TokenType tok;
     } reservedWords[MAXRESERVED]
-   = {{"if",IF},{"then",THEN},{"else",ELSE},{"end",END},
-      {"repeat",REPEAT},{"until",UNTIL},{"read",READ},
+   = {{"if",IF},{"else",ELSE},{"while",WHILE},{"return",RETURN},{"int",INT},{"void",VOID},
+      {"then",THEN},{"end",END},{"repeat",REPEAT},{"until",UNTIL},{"read",READ},
       {"write",WRITE}};
 
 /* lookup an identifier to see if it is a reserved word */
@@ -124,18 +124,30 @@ TokenType getToken(void)
              case '*':
                currentToken = TIMES;
                break;
-             case '/':
-               currentToken = OVER;
-               break;
              case '(':
                currentToken = LPAREN;
                break;
              case ')':
                currentToken = RPAREN;
                break;
+			 case '{':
+			   currentToken = LCURLY;
+			   break;
+			 case '}':
+			   currentToken = RCURLY;
+			   break;
+			 case '[':
+			   currentToken = LBRACE;
+			   break;
+			 case ']':
+			   currentToken = RBRACE;
+			   break;
              case ';':
                currentToken = SEMI;
                break;
+			 case ',':
+			   currentToken = COMMA;
+			   break;
              default:
                currentToken = ERROR;
                break;
